25082bee8ec73e317c3f63206c251026072ab261
alemalagni <alemalagni@gmail.com>
2025-05-16 16:45:04 +0200

    feat: add CustomerCard

--- src/components/CustomerCard.jsx
+++ src/components/CustomerCard.jsx
@@ -1,5 +1,12 @@
-function CustomerCard() {
-
+function CustomerCard({ id, nome, cognome, email, cellulare, codice_fiscale, data_nascita }) {
+    return (
+        <div>
+            <div>
+                <span>{nome}</span>
+                <span>{cognome}</span>
+            </div>
+        </div>
+    )
 }
 
 export default CustomerCard;

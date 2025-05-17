f6cc50ef15e2c7507fbbb18231f87a32d3b4b80e
willymariino <wg.mariino@gmail.com>
2025-05-17 13:06:47 +0200

    feat: added styling travel cards

--- src/pages/Viaggi.jsx
+++ src/pages/Viaggi.jsx
@@ -4,17 +4,25 @@ const Viaggi = () => {
 
 
     return <>
-        <div>
+        <section className="travels-container">
+
             <h1>Viaggi:</h1>
-            <div>
-                <label htmlFor="search">Cerca viaggi:</label>
-                <input type="text" id="search" />
+
+
+            <div className="card-container">
+
+
+                <form className="search-box">
+                    <label htmlFor="search">Cerca viaggi:</label>
+                    <input type="text" id="search" />
+                </form>
+
+                <div className="travels">
+                    {viaggi?.map(viaggio => <TravelCard key={viaggio.id} data={viaggio} />)}
+                </div>
             </div>
 
-            <div>
-                {viaggi?.map(viaggio => <TravelCard key={viaggio.id} data={viaggio}/>)}
-            </div>
-        </div>
+        </section>
     </>
 }
 

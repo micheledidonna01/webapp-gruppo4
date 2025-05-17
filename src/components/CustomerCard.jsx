b90cda87c39541d50794140ac82e4f02fe88ff08
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-05-17 13:06:09 +0200

    feat: refactor code ---> add btn to show/unshow customer details

--- src/components/CustomerCard.jsx
+++ src/components/CustomerCard.jsx
@@ -1,10 +1,29 @@
-function CustomerCard({ id, nome, cognome, email, cellulare, codice_fiscale, data_nascita }) {
+import { useState } from "react";
+
+
+function CustomerCard({ data }) {
+
+    const { nome, cognome, email, cellulare, codice_fiscale, data_nascita } = data;
+    const [isShow, setIsShow] = useState(false);
+
+    function setShow() {
+        setIsShow(!isShow);
+    }
+
     return (
         <div>
             <div>
-                <span>{nome}</span>
-                <span>{cognome}</span>
+                <span>{nome} {cognome}</span>
+                <button onClick={setShow}>Dettagli</button>
             </div>
+
+            {isShow && <ul>
+                <li>Data di nascita: {data_nascita}</li>
+                <li>Cellulare: {cellulare}</li>
+                <li>Email: {email}</li>
+                <li>Codice Fiscale: {codice_fiscale}</li>
+            </ul>}
+
         </div>
     )
 }

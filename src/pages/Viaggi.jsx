12376a98944656de431a5b06fe7613bc8717407d
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-05-17 12:58:25 +0200

    feat: create traverl filter

--- src/pages/Viaggi.jsx
+++ src/pages/Viaggi.jsx
@@ -1,18 +1,27 @@
 import viaggi from "../data/viaggi";
 import TravelCard from "../components/TravelCard";
+import { useState } from "react";
 const Viaggi = () => {
-
-
+    const [search, setSearch] = useState("");
+    const viaggiFiltrati = viaggi.filter(v =>
+        v.localita?.toLowerCase().includes(search.toLowerCase())
+    );
     return <>
         <div>
             <h1>Viaggi:</h1>
             <div>
                 <label htmlFor="search">Cerca viaggi:</label>
-                <input type="text" id="search" />
+                <input
+                    type="text"
+                    id="search"
+                    value={search}
+                    onChange={e => setSearch(e.target.value)}
+                />
             </div>
-
             <div>
-                {viaggi?.map(viaggio => <TravelCard key={viaggio.id} data={viaggio}/>)}
+                {viaggiFiltrati.map(viaggio => (
+                    <TravelCard key={viaggio.id} data={viaggio} />
+                ))}
             </div>
         </div>
     </>
